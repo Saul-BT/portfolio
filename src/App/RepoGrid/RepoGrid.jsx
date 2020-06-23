2b720a85105a603ad2b27335241f162d21b92034
Saul Blanco Tejero <saul.blanco.tejero@iesjulianmarias.es>
2020-06-23 14:21:27 +0200

    Unused things deleted

--- src/App/RepoGrid/RepoGrid.jsx
+++ src/App/RepoGrid/RepoGrid.jsx
@@ -1,20 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import RepoCard from './RepoCard'
 
-import info from '../../../info.json'
 import storedRepos from '../../../assets/repos.json'
 import './RepoGrid.scss'
-
-const username = info.username
-
-const getFilteredRepos = repos => {
-  Object.keys(info.repoFilters).forEach(key => {
-    repos = repos.filter(repo => {
-      return repo[key] === Boolean(info.repoFilters[key])
-    })
-  })
-  return repos
-}
 
 
 const RepoGrid = () => {
@@ -24,8 +12,7 @@ const RepoGrid = () => {
   
   return (
     <div className="repo-grid">
-      {repos
-	.map(repo => (
+      {repos.map(repo => (
 	  <RepoCard
 	    key={repo.name}
 	    url={repo.url}
